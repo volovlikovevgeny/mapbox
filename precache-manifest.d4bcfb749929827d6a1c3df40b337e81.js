583fd650ec46497cd67d7edd4e88f4223fc7573e
Evgeny Volovlikov <volovlikovevgeny@mail.ru>
2020-10-10 23:39:55 +0300

    Updates

---
+++ precache-manifest.d4bcfb749929827d6a1c3df40b337e81.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7f89ff5b01a109317139a63e0813d32",
+    "url": "/mapbox/index.html"
+  },
+  {
+    "revision": "ce81492c6f75418c588c",
+    "url": "/mapbox/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "8056ccdf917150919985",
+    "url": "/mapbox/static/css/main.bd808417.chunk.css"
+  },
+  {
+    "revision": "ce81492c6f75418c588c",
+    "url": "/mapbox/static/js/2.2dff9cf5.chunk.js"
+  },
+  {
+    "revision": "19a8633cc78222808709b0a6a7c8eae6",
+    "url": "/mapbox/static/js/2.2dff9cf5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8056ccdf917150919985",
+    "url": "/mapbox/static/js/main.57bab892.chunk.js"
+  },
+  {
+    "revision": "05e61796988d0fe3cc2a",
+    "url": "/mapbox/static/js/runtime-main.f1d00001.js"
+  }
+]);
